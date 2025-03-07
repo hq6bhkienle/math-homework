34089403ca698769f2390acf2ee668196d8f79b4
hq6bhkienle <hq6bhkienle@gmx.com>
2025-03-07 04:33:16 +0000

    Add 3.ts

---
+++ src/3.ts
@@ -0,0 +1,8 @@
+  import java.util.Random;
+
+public class MathHomework {
+    public static int getRandomNumber(int min, int max) {
+        Random rand = new Random();
+        return rand.nextInt((max - min) + 1) + min;
+    }
+}
